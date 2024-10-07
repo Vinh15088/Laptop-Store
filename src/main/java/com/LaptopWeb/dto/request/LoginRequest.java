865e131a3968d934ccce787dca0cd59fcbe6811f
Vinh15088 <vinhzz1508@gmail.com>
2024-10-07 23:27:19 +0700

    login, logout request

---
+++ src/main/java/com/LaptopWeb/dto/request/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.LaptopWeb.dto.request;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class LoginRequest {
+    String username;
+    String email;
+    String password;
+}
