dba7d20e5f8853c4bff5cf93e790672a3888a3d1
Vinh15088 <vinhzz1508@gmail.com>
2024-10-14 16:53:26 +0700

    update

--- src/main/java/com/LaptopWeb/repository/OrderStatusRepository.java
+++ src/main/java/com/LaptopWeb/repository/OrderStatusRepository.java
@@ -2,9 +2,11 @@ package com.LaptopWeb.repository;
 
 import com.LaptopWeb.entity.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface OrderStatusRepository extends JpaRepository<OrderStatus, Integer> {
 
     Optional<OrderStatus> findByName(String name);
